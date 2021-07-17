a62c183dc8395e01f168a5dc030fecce7c342c33
Leanne <leanne_porter@hotmail.com>
2021-07-17 11:24:25 +1000

    adding code to app section

---
+++ app.py
@@ -0,0 +1,29 @@
+# Import Setup and Dependencies
+import datetime as dt 
+import numpy as np 
+import pandas as pd 
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+
+from flask import Flask, jsonify
+
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Flash Setup
+
+
