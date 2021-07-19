c3cbcb9be92904820011c7f633d5cb79e72aa526
Leanne <leanne_porter@hotmail.com>
2021-07-19 13:48:02 +1000

    finalising code

--- app.py
+++ app.py
@@ -10,14 +10,13 @@ from sqlalchemy import create_engine, func, inspect
 
 from flask import Flask, jsonify
 
-
+# Setting up the database
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
-
-# reflect an existing database into a new model
+# Reflect an existing database into a new model
 Base = automap_base()
 
-# reflect the tables
+# Reflect the tables
 Base.prepare(engine, reflect=True)
 
 # Save references to each table
@@ -25,11 +24,11 @@ Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 # Flask Setup
-
 app = Flask(__name__)
 
 # Flask Routes
 
+# Homepage which lists all the available routes.
 @app.route("/")
 def welcome():
     return (
@@ -42,6 +41,8 @@ def welcome():
         f"/api/v1.0/start/end<br/>"
     )
 
+
+# Precipiation route which returns the years precipation results and displays the date and prcp value.
 @app.route("/api/v1.0/precipitation")
 def precipitation():
 
@@ -56,6 +57,7 @@ def precipitation():
     return jsonify(precip)
 
 
+# Station route which returns a list of stations from the dataset.
 @app.route("/api/v1.0/stations") 
 def stations():
 
@@ -70,6 +72,7 @@ def stations():
     return jsonify(station_id)
 
 
+# TOBS route which returns all temperature observations for the previous year filtered on the most active station.
 @app.route("/api/v1.0/tobs")
 def tobs():
 
@@ -87,6 +90,9 @@ def tobs():
     year_tobs = {date: tobs for date, tobs in temp_data}
     return jsonify(year_tobs)
 
+
+# This route allows users to input a start date and returns the average, minimum and maximum TOBS for the range.
+# The range is all dates after the start date.
 @app.route("/api/v1.0/<start>")
 def start_date(start):
 
@@ -109,6 +115,7 @@ def start_date(start):
 
     return jsonify(all_tobs)
 
+# This route allows users to input a start and end date and returns the average, minimum and maximum TOBS for the defined date range.
 @app.route("/api/v1.0/<start>/<end>")
 def start_end_date(start, end):
 
